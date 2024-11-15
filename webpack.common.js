66a6ce9cc6d8effdc77253ca1569b532c9b9d3a4
eugene.levenetc <1069159+elevenetc@users.noreply.github.com>
2024-11-15 17:42:29 +0100

    Add type TS config

--- webpack.common.js
+++ webpack.common.js
@@ -9,4 +9,20 @@ module.exports = {
     clean: true,
     filename: './js/app.js',
   },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"],
+    // Add support for TypeScripts fully qualified ESM imports.
+    extensionAlias: {
+      ".js": [".js", ".ts"],
+      ".cjs": [".cjs", ".cts"],
+      ".mjs": [".mjs", ".mts"]
+    }
+  },
+  module: {
+    rules: [
+      // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+      {test: /\.([cm]?ts|tsx)$/, loader: "ts-loader"}
+    ]
+  }
 };
